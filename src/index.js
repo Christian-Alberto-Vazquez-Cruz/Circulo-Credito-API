48ff860a2ab3c7822a9ee3ef9a8540f2ee328b10
magik <zS22013636@estudiantes.uv.mx>
2026-01-03 22:56:25 -0600

    Buro: Cambio en Router getResumenCrediticioPorRFC

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import validarApiKey from './middleware/validarApiKey.js'
 import personasRouter from './routes/personas.route.js'
 import obligacionesRouter from  './routes/obligaciones.route.js'
 import pagosRouter from './routes/pagos.route.js'
+import buroRoute from './routes/buro.route.js'
 
 import { prisma } from './lib/db.js'
 
@@ -20,5 +21,6 @@ app.get("/consulta", async (req, res) => {
 app.use("/personas", personasRouter)
 app.use("/obligaciones", obligacionesRouter)
 app.use("/pagos", pagosRouter)
+app.use("/buro", buroRoute)
 
 app.listen(3000, () => console.log("API en puerto 3000"));
--- src/routes/buro.route.js
+++ src/routes/buro.route.js
@@ -2,6 +2,6 @@ import { Router } from 'express'
 import { BuroController } from '../controller/buro.controller.js'
 const router = Router()
 
-router.get("resumen/:rfc", BuroController.getResumenBuroPorRFC)
+router.get("resumen/:rfc", BuroController.getResumenCrediticioPorRFC)
 
 export default router
