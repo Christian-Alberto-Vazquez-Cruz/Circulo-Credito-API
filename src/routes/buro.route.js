d800ee9a50467362ca69d615a2f3724f0aa04e97
magik <zS22013636@estudiantes.uv.mx>
2026-01-05 04:49:16 -0600

    fix ruta buro

--- src/routes/buro.route.js
+++ src/routes/buro.route.js
@@ -2,6 +2,6 @@ import { Router } from 'express'
 import { BuroController } from '../controller/buro.controller.js'
 const router = Router()
 
-router.get("resumen/:rfc", BuroController.getResumenCrediticioPorRFC)
+router.get("/resumen/:rfc", BuroController.getResumenCrediticioPorRFC)
 
 export default router
--- src/routes/pagos.route.js
+++ src/routes/pagos.route.js
@@ -2,8 +2,8 @@ import { Router } from 'express'
 import { PagosController } from '../controller/pagos.controller.js'
 const router = Router()
 
-router.get("/:rfc", PagosController.listarPagos)
 router.get("/pendientes/:rfc", PagosController.pagosPendientes)
 router.get("/estadisticas/:rfc", PagosController.getEstadisticasPagoPorRFC)    
+router.get("/:rfc", PagosController.listarPagos)
 
 export default router;
